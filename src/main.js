34c94f633b351b1e438d6b378c3863bb773a561d
zenzenlin <poietgr0107@gmail.com>
2021-06-14 22:58:25 +0800

    update2

--- src/main.js
+++
@@ -1,1 +0,0 @@
-const myModule = require('./module/demo.js')
--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-var path = require('path')
-
-module.exports = {
-  entry: ['./src/main.js'], // 在 index 檔案後的 .js 副檔名是可選的
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js'
-    }
-}
